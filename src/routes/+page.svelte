14e6a2f00488fd1b84b3dcfd86923f5a0104e09a
Taylor Breutzmann <tbrut@protonmail.com>
2026-02-02 12:34:34 -0500

    ..

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Chicago Boat Rentals – The Best Boats, Yachts, and Experiences in Chicago</h1>
+
+<p>
+	We're CHicago Boating Hub. Partnered with the leading chicago boat rental companies in Chicago. We
+	provide a platform for customers to find and book the perfect boat for their needs. Our partners
+	are vetted and insured, and we offer a seamless booking experience.
+</p>
